621fa2dd373bb9170ccd47ed87844a07dcf6b8db
Alhanouf0 <97540130+Alhanouf0@users.noreply.github.com>
2022-02-12 19:12:09 +0300

    Revert "Features"

--- app/mailers/item_mailer.rb
+++
@@ -1,9 +0,0 @@
-class ItemMailer < ApplicationMailer
-
-    def new_item_mail
-        @item = params[:item]
-        @user = User.all
-        
-        mail(to:@user.map{|u| [u.email]}, subject: 'new item added')
-    end
-end
